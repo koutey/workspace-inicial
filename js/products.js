98651f75637d2d9dc1c5743abe3e12466562e590
koutey <nicotab45@gmail.com>
2022-08-21 14:44:39 -0300

    aaa

--- js/products.js
+++ js/products.js
@@ -1,43 +1,41 @@
-let categoriesArray = [];
+const products = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-function showCategoriesList(){
+let productsArray = [];
+
+function showProductsList(){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCategoriesArray.length; i++){
-        let category = currentCategoriesArray[i];
-
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+    for(let i = 0; i < productsArray.length; i++){
+        let product = productsArray[i];
 
             htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+            <div  class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
+                        <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name}</h4>
-                            <small class="text-muted">${category.productCount} artículos</small>
+                            <h4 class="mb-1">${product.name} - ${product.currency} - ${product.cost}</h4>
+                            <small class="text-muted">${product.soldCount} artículos</small>
                         </div>
-                        <p class="mb-1">${category.description}</p>
+                        <p class="mb-1">${product.description}</p>
                     </div>
                 </div>
             </div>
             `
-        }
+        
 
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
     }
 }
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(products).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data.products;
+            showProductsList(productsArray);
         }
     });
 });
